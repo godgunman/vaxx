70cd4303b6931312441402f471423bb40f9e366a
ggm <godgunman@gmail.com>
2021-07-12 00:46:58 +0800

    Set district and phone to optional

--- src/types.ts
+++ src/types.ts
@@ -1,9 +1,12 @@
+// 1. 必要欄位：施打站全稱、施打站縣市、施打站地址
+// 2. 選擇欄位：施打站行政區、施打站地址、官方提供網址、預約電話、醫事機構代碼、施打站經度、施打站緯度
+
 export type Place = {
   city: string;
   name: string;
-  district: string;
   address: string;
-  phone: string;
+  district?: string;
+  phone?: string;
   lng?: number;
   lat?: number;
   specialty?: string;
