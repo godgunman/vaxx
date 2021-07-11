6669584eb3386805aa67002b131d0e8084892c43
ggm <godgunman@gmail.com>
2021-07-12 00:33:41 +0800

    Return lat, lng

--- src/libs.ts
+++ src/libs.ts
@@ -1,10 +1,12 @@
+/* eslint-disable camelcase */
 import { GOOGLE_API_KEY } from './constants';
 import fetch from 'node-fetch';
+import { Place } from './types';
 
 export const jsonToCsv = (
   keys: string[],
   csvKeysMap: string[],
-  data: object[],
+  data: Place[],
 ) => {
   const rows = [csvKeysMap.join(',')];
   for (const obj of data) {
@@ -14,7 +16,15 @@ export const jsonToCsv = (
   return rows;
 };
 
-export const getPlaceInfo = async (name: string) => {
+export const getPlaceInfo = async (
+  name: string,
+): Promise<{
+  address: string;
+  phone: string;
+  district: string;
+  lat: string;
+  lng: string;
+}> => {
   const encodedName = encodeURI(name);
   const findPlaceFromText = `https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=${encodedName}&inputtype=textquery&key=${GOOGLE_API_KEY}`;
 
@@ -23,12 +33,18 @@ export const getPlaceInfo = async (name: string) => {
   const details = `https://maps.googleapis.com/maps/api/place/details/json?place_id=${placeId}&key=${GOOGLE_API_KEY}&language=zh-TW`;
 
   const {
-    // eslint-disable-next-line camelcase
-    result: { formatted_address, formatted_phone_number, address_components },
+    result: {
+      formatted_address,
+      formatted_phone_number,
+      address_components,
+      geometry: {
+        location: { lat, lng },
+      },
+    },
   } = await (await fetch(details)).json();
 
   let district = '';
-  // eslint-disable-next-line camelcase
+
   for (const component of address_components) {
     if (component.types.indexOf('administrative_area_level_3') !== -1) {
       district = component.long_name;
@@ -39,5 +55,7 @@ export const getPlaceInfo = async (name: string) => {
     address: formatted_address,
     phone: formatted_phone_number,
     district,
+    lat,
+    lng,
   };
 };
