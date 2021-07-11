a6879870419db371b168f62dbe9c15cf44003f53
ggm <godgunman@gmail.com>
2021-07-11 21:39:29 +0800

    Add getPlaceInfo()

--- src/libs.ts
+++ src/libs.ts
@@ -1,3 +1,6 @@
+import { GOOGLE_API_KEY } from './constants';
+import fetch from 'node-fetch';
+
 export const jsonToCsv = (
   keys: string[],
   csvKeysMap: string[],
@@ -9,4 +12,32 @@ export const jsonToCsv = (
     rows.push(row);
   }
   return rows;
+};
+
+export const getPlaceInfo = async (name: string) => {
+  const encodedName = encodeURI(name);
+  const findPlaceFromText = `https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=${encodedName}&inputtype=textquery&key=${GOOGLE_API_KEY}`;
+
+  const { candidates } = await (await fetch(findPlaceFromText)).json();
+  const placeId = candidates[0].place_id;
+  const details = `https://maps.googleapis.com/maps/api/place/details/json?place_id=${placeId}&key=${GOOGLE_API_KEY}&language=zh-TW`;
+
+  const {
+    // eslint-disable-next-line camelcase
+    result: { formatted_address, formatted_phone_number, address_components },
+  } = await (await fetch(details)).json();
+
+  let district = '';
+  // eslint-disable-next-line camelcase
+  for (const component of address_components) {
+    if (component.types.indexOf('administrative_area_level_3') !== -1) {
+      district = component.long_name;
+    }
+  }
+
+  return {
+    address: formatted_address,
+    phone: formatted_phone_number,
+    district,
+  };
 };
