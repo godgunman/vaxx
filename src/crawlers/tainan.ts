ba3291bef309ece93e363fc5499b466d9b37c1f8
ggm <godgunman@gmail.com>
2021-07-11 19:17:30 +0800

    Fix name field

--- src/crawlers/tainan.ts
+++ src/crawlers/tainan.ts
@@ -31,30 +31,30 @@ pdf(dataBuffer).then(function (data) {
   // console.log(lines.map((e, index) => `${index}:${e}`).join('\n'));
 
   // 臺南市COVID-19疫苗接種合約醫院名冊
-  let lastHospital = false;
+
   const csvKeys = [
     'city',
-    'specialty',
-    'serial',
     'name',
     'district',
     'address',
     'phone',
-    'note',
-    'department',
+    // 'specialty',
+    // 'serial',
+    // 'note',
+    // 'department',
   ];
   // 1. 必要欄位：施打站全稱、施打站縣市、施打站地址
   // 2. 選擇欄位：施打站行政區、施打站地址、官方提供網址、預約電話、醫事機構代碼、施打站經度、施打站緯度
   const csvKeysMap = [
     '施打站縣市',
-    'specialty',
-    'serial',
     '施打站全稱',
     '施打站行政區',
     '施打站地址',
     '預約電話',
-    'note',
-    'department',
+    // 'specialty',
+    // 'serial',
+    // 'note',
+    // 'department',
   ];
   const results = [];
   for (let i = 0; i < lines.length; i++) {
@@ -64,22 +64,31 @@ pdf(dataBuffer).then(function (data) {
       const city = '台南市';
       const specialty = lines[i] + lines[i + 1];
       const serial = lines[j + 2];
-      if (serial === '25') lastHospital = true;
       const district = lines[j + 3];
-      const name = lines[j + 4];
-      j = j + 5;
-      let address = '';
-      while (lines[j].startsWith('06') === false) {
-        address += lines[j++];
+
+      j = j + 4;
+      let name = '';
+      while (!lines[j].startsWith('台南市') && !lines[j].startsWith('7')) {
+        name += lines[j++];
       }
-      const phone = lines[j];
+
+      let address = '';
+      let phone = '';
+      if (lines[j].indexOf('06-') !== -1) {
+        [address, phone] = lines[j].split('06-');
+      } else {
+        while (!lines[j].startsWith('06-')) {
+          address += lines[j++];
+        }
+        phone = lines[j];
+      }
 
       // 掛號科別
       const department = lines[j + 1];
       j = j + 2;
       let note = '';
 
-      while (lines[j].indexOf('專責') === -1) {
+      while (lines[j] && lines[j].indexOf('專責') === -1) {
         note += lines[j++];
       }
 
@@ -95,10 +104,8 @@ pdf(dataBuffer).then(function (data) {
         department,
       };
       results.push(result);
-      // console.log(i, result);
     }
-    if (lastHospital) break;
   }
-  console.log(JSON.stringify(results, null, 2));
-  // console.log(jsonToCsv(csvKeys, csvKeysMap, results).join('\n'));
+  // console.log(JSON.stringify(results, null, 2));
+  console.log(jsonToCsv(csvKeys, csvKeysMap, results).join('\n'));
 });
