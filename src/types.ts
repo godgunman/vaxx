b9ce551c73db875b9dd3536a1000495e0bbd0f19
ggm <godgunman@gmail.com>
2021-07-12 00:33:17 +0800

    Add type Place

---
+++ src/types.ts
@@ -0,0 +1,13 @@
+export type Place = {
+  city: string;
+  name: string;
+  district: string;
+  address: string;
+  phone: string;
+  lng?: string;
+  lat?: string;
+  specialty?: string;
+  serial?: string;
+  note?: string;
+  department?: string;
+};
